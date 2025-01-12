7777fa8e8e6d096451434476ef472fbc61ca1957
Ron Dumalagan <43971216+ronduma@users.noreply.github.com>
2025-01-12 00:19:51 -0500

    dark mode stuff

--- ronduma.github.io/src/App.tsx
+++ ronduma.github.io/src/App.tsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState, useEffect} from 'react';
 import './App.css'
 
+// components
+import CustomSwitch from './components/Switch'
+
+// mui components
+import { 
+  Grid2 as Grid,
+} from '@mui/material'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkMode, setDarkMode] = useState(() => window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const handleChange = (e: MediaQueryListEvent) => setDarkMode(e.matches);
+    mediaQuery.addEventListener('change', handleChange);
+    return () => mediaQuery.removeEventListener('change', handleChange);
+  }, []);
+
+  const handleModeChange = (newMode: boolean) => {
+    setDarkMode(newMode);
+    console.log('Mode changed to:', newMode);
+    document.documentElement.setAttribute('data-theme', newMode ? 'dark' : 'light');
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Grid>
+        <CustomSwitch mode={darkMode} onModeChange={handleModeChange} />
+        {darkMode ? 'Dark Mode' : 'Light Mode'}
+      </Grid>
     </>
   )
 }
